1371fd1d2affe2c9d9f9afd5fb674a696071184e
Kristine Ann Maglinao <kristinemaglinao06@gmail.com>
2025-10-05 21:24:58 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const title = document.querySelector('.ksstn-title');
+    if (title) {
+        // Add a class for the shine effect
+        title.classList.add('ksstn-shine');
+    }
+});
